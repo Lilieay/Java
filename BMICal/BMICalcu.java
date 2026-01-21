bf3961661cf4ff896e7efb15d8b55d7e32fb7aba
phongsapak <ketphunt4@gmail.com>
2026-01-21 21:13:26 +0700

    try package

---
+++ BMICal/BMICalcu.java
@@ -0,0 +1,10 @@
+package BMICal;
+import java.util.Scanner;
+public class BMICalcu {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter weight (kg): ");
+        double weight = scan.nextDouble();
+
+    }
+}
--- HelloWorld/Greeter.java
+++ HelloWorld/Greeter.java
@@ -1,3 +1,4 @@
+package HelloWorld;
 public class Greeter {
     public void say(String str) {
         System.out.printf("%s",str);
