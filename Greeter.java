467b2a96414ff1e4990eb46ecf32562622c20243
phongsapak <ketphunt4@gmail.com>
2026-01-21 13:36:53 +0700

    my first java hello world

---
+++ Greeter.java
@@ -0,0 +1,5 @@
+public class Greeter {
+    public void say(String str) {
+        System.out.printf("%s",str);
+    }
+}
---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        Greeter greet = new Greeter();
+        greet.say("Hello World");
+    }
+}
